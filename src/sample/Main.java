f668b755f8801859f4a7bb9b8e9e27332811df96
guleri <abhishekguleri24@gmail.com>
2019-08-19 23:39:22 +0530

    Graphics project

---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Alert.AlertType;
+
+public class Main extends Application {
+Button button;
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+         root = FXMLLoader.load(getClass().getResource("ChoiceLayout.fxml"));
+
+        button = new Button("click me ");
+
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
