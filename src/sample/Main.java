29d3dccf865253aa88175d6a97d2438f17dd5a09
Abhishek Guleri <43719098+Guleri24@users.noreply.github.com>
2019-08-25 23:29:22 +0530

    text translateTransition added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,22 +1,21 @@
 package sample;
 
-import com.sun.xml.internal.bind.WhiteSpaceProcessor;
 import javafx.animation.RotateTransition;
-import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
 import javafx.application.Application;
-import javafx.scene.DepthTest;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.effect.*;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Ellipse;
+import javafx.scene.effect.ImageInput;
+import javafx.scene.image.Image;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Scale;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -30,6 +29,7 @@ public class Main extends Application {
     rectangle.setFill(DARKTURQUOISE);
 rectangle.setArcHeight(35);
 rectangle.setArcWidth(35);
+
     Rectangle rectangle1 = new Rectangle(100,100,1150,500);
       rectangle1.setFill(WHITE);
     rectangle1.setArcHeight(35);
@@ -40,9 +40,23 @@ rectangle.setArcWidth(35);
     text.setY(200);
     text.setFont(Font.font("Times Roman",FontWeight.BOLD, FontPosture.ITALIC,80));
     text.setFill(ORANGE);
-    text.setText("SCHOOL ----" +
-            "\n   Six Cruel Hours \n\t\t Of Our Life ");
+    text.setText("SCHOOL ---- \n   Six Cruel Hours \n\t\t Of Our Life ");
 
+/* rotateTransition.setAxis(Rotate.Z_AXIS);
+    rotateTransition.setByAngle(360);
+    rotateTransition.setCycleCount(500);
+      rotateTransition.setDuration(Duration.millis(1000));
+    rotateTransition.setAutoReverse(true);
+    rotateTransition.setNode(rectangle1);
+    rotateTransition.play();*/
+      TranslateTransition translateTransition = new TranslateTransition();
+
+      translateTransition.setByY(100);
+      translateTransition.setDuration(Duration.millis(4000));
+      translateTransition.setCycleCount(100);
+      translateTransition.setAutoReverse(true);
+      translateTransition.setNode(text);
+      translateTransition.play();
 
       Group group = new Group();
       group.getChildren().addAll(rectangle,rectangle1,text);
