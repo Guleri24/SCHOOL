c5ae82d741eb0f475f5e0e0a658ec5519504f202
Abhishek Guleri <43719098+Guleri24@users.noreply.github.com>
2019-08-27 22:06:53 +0530

    main scene ready and added

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,21 +1,20 @@
 package sample;
 
-import javafx.animation.RotateTransition;
+import javafx.animation.FillTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.StrokeTransition;
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.effect.ImageInput;
-import javafx.scene.image.Image;
-import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.CullFace;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Sphere;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Scale;
-import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -30,19 +29,37 @@ public class Main extends Application {
 rectangle.setArcHeight(35);
 rectangle.setArcWidth(35);
 
+
     Rectangle rectangle1 = new Rectangle(100,100,1150,500);
       rectangle1.setFill(WHITE);
     rectangle1.setArcHeight(35);
     rectangle1.setArcWidth(35);
 
-    Text text = new Text();
+      Sphere sphere2 = new Sphere();
+      sphere2.setRadius(50);
+      sphere2.setTranslateX(1000);
+      sphere2.setTranslateY(550);
+      ScaleTransition scaleTransition = new ScaleTransition();
+      scaleTransition.setDuration(Duration.millis(1000));
+      scaleTransition.setFromX(2);
+      scaleTransition.setAutoReverse(true);
+      scaleTransition.setCycleCount(1000);
+      scaleTransition.setByX(2);
+      scaleTransition.setToX(1);
+      scaleTransition.setNode(sphere2);
+      scaleTransition.play();
+
+      //setting CullFace for the sphere 3
+      sphere2.setCullFace(CullFace.NONE);
+
+      Text text = new Text();
     text.setX(160);
     text.setY(200);
     text.setFont(Font.font("Times Roman",FontWeight.BOLD, FontPosture.ITALIC,80));
     text.setFill(ORANGE);
     text.setText("SCHOOL ---- \n   Six Cruel Hours \n\t\t Of Our Life ");
 
-/* rotateTransition.setAxis(Rotate.Z_AXIS);
+ /* rotateTransition.setAxis(Rotate.Z_AXIS);
     rotateTransition.setByAngle(360);
     rotateTransition.setCycleCount(500);
       rotateTransition.setDuration(Duration.millis(1000));
@@ -58,13 +75,25 @@ rectangle.setArcWidth(35);
       translateTransition.setNode(text);
       translateTransition.play();
 
+      FillTransition fillTransition = new FillTransition();
+      fillTransition.setAutoReverse(true);
+      fillTransition.setCycleCount(100);
+      fillTransition.setDuration(Duration.millis(2000));
+      fillTransition.setToValue(BLUEVIOLET);
+      fillTransition.setShape(rectangle);
+      fillTransition.play();
+
       Group group = new Group();
-      group.getChildren().addAll(rectangle,rectangle1,text);
+      group.getChildren().addAll(rectangle,rectangle1,text,sphere2);
       Scene scene = new Scene(group,1400,1200,DARKGRAY);
+
+
       primaryStage.setScene(scene);
       primaryStage.setTitle("School");
       primaryStage.show();
-    }
+  }
+
+
 
 
     public static void main(String[] args) {
